436648b57e2e1c739c4330032f90cc948f059469
Kuro <kyawcthumoeoo@gmail.com>
2024-08-26 03:00:12 +0700

    Add LoadingState

--- app/src/main/java/com/ryan/weather/util/WResult.kt
+++ app/src/main/java/com/ryan/weather/util/WResult.kt
@@ -5,4 +5,6 @@ import com.ryan.weather.core.data.RemoteSourceException
 sealed class WResult<out T> {
     data class Success<out T>(val data: T) : WResult<T>()
     data class Failure(val error: RemoteSourceException) : WResult<Nothing>()
+    data object Loading : WResult<Nothing>()
+
 }
