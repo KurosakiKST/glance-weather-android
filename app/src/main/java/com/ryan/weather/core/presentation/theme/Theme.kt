26538fab4d50d20488322771b1c646d94f79f223
Kuro <kyawcthumoeoo@gmail.com>
2024-08-26 00:27:31 +0700

    Add Text Composable

--- app/src/main/java/com/ryan/weather/core/presentation/theme/Theme.kt
+++ app/src/main/java/com/ryan/weather/core/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.ryan.weather.core.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun CodigocodingtestTheme(
+fun WeatherAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
