ae25453ffad39a531075f6c7c6fe802254c54047
Kuro <kyawcthumoeoo@gmail.com>
2024-08-27 15:35:24 +0700

    Refactor Constants Refactor Request Error handling

---
+++ app/src/main/java/com/ryan/weather/core/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.ryan.weather.core.utils
+
+object Constants {
+    const val API_KEY = "f500c2f4a97b47a5a86144643242408"
+    const val BASE_URL = "https://api.weatherapi.com"
+}
+
+object NetWorkService {
+    const val BASE_URL = "https://api.weatherapi.com"
+    const val API_KEY = "f500c2f4a97b47a5a86144643242408"
+    const val CURRENT_WEATHER = "/v1/current.json"
+    const val FORECAST_WEATHER = "/v1/forecast.json"
+    const val CITIES = "/v1/search.json"
+}
+
+object Database {
+    const val DATABASE_NAME = "weather_data.db"
+}
