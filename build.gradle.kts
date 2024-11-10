6637237d223c33a5d17ba85e4ce53d3f7d18732b
Kuro <kyawcthumoeoo@gmail.com>
2024-11-10 22:51:28 +0700

    Add Firebase App Distribution Update CICD Flow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.google.firebase:firebase-appdistribution-gradle:4.0.1")
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.hilt.plugin) apply false
-    alias(libs.plugins.android.library) apply false
 }
