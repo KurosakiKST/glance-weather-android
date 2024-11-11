956e2f1de403261e0203b198d42fff7f425b35e3
Kuro <kyawcthumoeoo@gmail.com>
2024-11-12 00:28:23 +0700

    Refactor API Key and URL to BuildConfig

--- app/src/main/java/com/ryan/weather/core/presentation/utils/Constants.kt
+++ app/src/main/java/com/ryan/weather/core/presentation/utils/Constants.kt
@@ -1,13 +1,10 @@
 package com.ryan.weather.core.presentation.utils
 
-object Constants {
-    const val API_KEY = "f500c2f4a97b47a5a86144643242408"
-    const val BASE_URL = "https://api.weatherapi.com"
-}
+import com.ryan.weather.BuildConfig
 
 object NetWorkService {
-    const val BASE_URL = "https://api.weatherapi.com"
-    const val API_KEY = "f500c2f4a97b47a5a86144643242408"
+    val BASE_URL: String = BuildConfig.BASE_URL
+    val API_KEY: String = BuildConfig.API_KEY
     const val CURRENT_WEATHER = "/v1/current.json"
     const val FORECAST_WEATHER = "/v1/forecast.json"
     const val CITIES = "/v1/search.json"
