0d9d1b17a1a06ca5fa8d0894ada7f6a04c708ec0
Kuro <kyawcthumoeoo@gmail.com>
2024-11-20 15:42:38 +0700

    Separate Detail and Home Screen

--- app/src/main/java/com/ryan/weather/weather/presentation/detail/components/WeatherItem.kt
+++ app/src/main/java/com/ryan/weather/weather/presentation/detail/components/WeatherItem.kt
@@ -1,4 +1,4 @@
-package com.ryan.weather.weather.presentation.home.components
+package com.ryan.weather.weather.presentation.detail.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
